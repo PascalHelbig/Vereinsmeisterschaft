72480bc22c9d8476455782fbbc69f5dcf4e4f64e
PascalHelbig <pascalhelbig@gmail.com>
2018-12-03 15:51:25 +0100

    creat club as own graphql type

--- plugins/gatsby-source-raceoffice/gatsby-node.js
+++ plugins/gatsby-source-raceoffice/gatsby-node.js
@@ -9,6 +9,46 @@ exports.sourceNodes = async ({
   createNodeId,
   createContentDigest,
 }) => {
+  let clubObject = {};
+
+  /**
+   * Adds an club to clubObject
+   *
+   * @param {string} club Club short name
+   * @param {string} clubCode DSV club code
+   * @param {string} resultId the node id of the result
+   * @param {boolean} forHelm add result for helm?
+   *
+   * @returns {string} the club id for GraphQL
+   */
+  const addClub = (club, clubCode, resultId, forHelm) => {
+    const clubId = createNodeId(`raceoffice-club-${club}-${clubCode}`);
+
+    if (!clubObject[clubId]) {
+      clubObject = {
+        [clubId]: {
+          name: club,
+          dsvCode: clubCode,
+          allResults___NODE: [],
+          helmResults___NODE: [],
+          crewResults___NODE: [],
+        },
+        ...clubObject,
+      };
+    }
+
+    if (!clubObject[clubId].allResults___NODE.includes(resultId)) {
+      clubObject[clubId].allResults___NODE.push(resultId);
+    }
+    if (forHelm) {
+      clubObject[clubId].helmResults___NODE.push(resultId);
+    } else {
+      clubObject[clubId].crewResults___NODE.push(resultId);
+    }
+
+    return clubId;
+  };
+
   const ranglisteHTML = await fetchRangliste(2018);
   const regattaList = getRegattas(ranglisteHTML);
 
@@ -23,13 +63,30 @@ exports.sourceNodes = async ({
       const results = getResults(resultHTML);
 
       resultIds = results.map((result, index) => {
-        const resultObject = {
-          ...result,
-          regatta___NODE: regattaId,
-        };
         const resultId = createNodeId(
           `raceoffice-regatta-${regatta.url}-result-${index}`,
         );
+
+        const helmClubId = addClub(
+          result.helmClub,
+          result.helmClubCode,
+          resultId,
+          true,
+        );
+        const crewClubId = addClub(
+          result.crewClub,
+          result.crewClubCode,
+          resultId,
+          false,
+        );
+
+        const resultObject = {
+          ...result,
+          helmClub___NODE: helmClubId,
+          crewClub___NODE: crewClubId,
+          regatta___NODE: regattaId,
+        };
+
         const resultNode = {
           ...resultObject,
           id: resultId,
@@ -63,5 +120,21 @@ exports.sourceNodes = async ({
     createNode(regattaNodeData);
   });
 
-  return Promise.all(regattaPromises);
+  return Promise.all(regattaPromises).then(() => {
+    Object.keys(clubObject).forEach(clubId => {
+      const club = clubObject[clubId];
+      const clubNodeData = {
+        ...club,
+        id: clubId,
+        parent: null,
+        children: [],
+        internal: {
+          type: 'RaceofficeClub',
+          content: JSON.stringify(club),
+          contentDigest: createContentDigest(club),
+        },
+      };
+      createNode(clubNodeData);
+    });
+  });
 };
